90779eeefb5d30f62b457e4af85176e773f81067
—Matt <38044816+mattcapazz@users.noreply.github.com>
2021-11-11 16:24:14 +0000

    Added URL detection logic & Changed logging
    With these changes, the bot can now verify if messages contain the same link as the message that got flagged as spam.
    
    Example: 
    • free discord nitro http://discord.freegift.xyz
    • discord nitro here: http://discord.freegift.xyz
    
    If one of them is marked as spam, both will get removed.

--- index.js
+++ index.js
@@ -1,53 +1,56 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const { Intents, Client, Permissions } = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
-client.login(process.env.BOT_TOKEN)
+const { Intents, Client, Permissions } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 client.once("ready", () => {
-    console.log("ready");
-
-})
+  console.log("Ready");
+});
 
 client.on("messageCreate", (msg) => {
-    if (msg.author.bot) return false;
+  if (msg.author.bot) return false;
+  if (msg.content == "spam" && msg.type == "REPLY") {
+    try {
+      // Only allow “admins” to mark message as spam
+      if (msg.member.permissions.has([Permissions.FLAGS.KICK_MEMBER])) {
+        let spamId = msg.reference.messageId;
 
-    if (msg.content == "spam" && msg.type == "REPLY") {
+        msg.channel.messages.fetch(spamId).then((m) => {
+          // RegEx exp. from: geeksforgeeks.org/how-to-validate-url-using-regular-expression-in-javascript
+          let spamMsg = m.content,
+            channels = client.channels.cache,
+            regex =
+              /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi,
+            url = spamMsg.match(regex);
 
-        try {
-            // only allowing admins to mark spam
-            if (msg.member.permissions.has([Permissions.FLAGS.KICK_MEMBER])) {
-
-                spamId = msg.reference.messageId;
-
-                msg.channel.messages.fetch(spamId).then(val => {
-                    let spamMsg = val.content;
-
-                    let channels = client.channels.cache;
-
-                    for (const channelObj of channels) {
-                        channelId = channelObj[0];
-
-                        channel = client.channels.resolve(channelId)
-                        if (channel.isText()) {
-                            cache = client.channels.cache.get(channelId)
-                            cache.messages.fetch({ limit: 40 }).then(
-                                async msg => {
-                                    msg.each(messg => {
-                                        if (messg.content == spamMsg) {
-                                            messg.delete();
-                                        }
-                                    })
-                                }
-                            )
-                        }
-                    }
+          for (const channelObj of channels) {
+            channelId = channelObj[0];
+            channel = client.channels.resolve(channelId);
+            if (channel.isText()) {
+              cache = client.channels.cache.get(channelId);
+              cache.messages.fetch({ limit: 40 }).then(async (msg) => {
+                msg.each((m) => {
+                  if (m.content == spamMsg || m.content.includes(url))
+                    /* If the message fetched is equal to the msg marked as spam AND contains the same link
+                    from it, deletes the message. */
+                    m.delete();
                 });
+              });
             }
-        }
-        catch {
-            console.log("Not admin");
-        }
+          }
+        });
+      } else
+        console.log(
+          `${msg.author.tag} tried flagging a message as spam without the “KICK_MEMBER” perm.`
+        );
+
+      msg.delete();
+    } catch (err) {
+      console.error(err);
     }
+  }
 });
+
+client.login(process.env.BOT_TOKEN);
